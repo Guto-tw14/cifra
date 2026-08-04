b88712a700e4b03072e58c3ca112ec277f4de79c
Guto <augusto.twainer@gmail.com>
2026-08-03 21:52:36 -0300

    interface inicial

---
+++ app/cifra.tsx
@@ -0,0 +1,8 @@
+export class Cifra {
+    public nome: string
+    public link: string
+    constructor(nome: string, link: string){
+        this.nome = nome
+        this.link = link
+    }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,147 @@
-import Image from "next/image";
+'use client';
+import React, { ReactNode } from 'react';
+import Image from 'next/image';
+import { Cifra } from '@/app/cifra';
+import { useState } from 'react';
 
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+type IconeProps = {
+    src: string;
+    alt: string;
+    x: number;
+    y: number;
+};
+function Icone({ src, alt, x, y }: IconeProps) {
+    return <Image src={src} alt={alt} width={x} height={y} draggable={false} />;
+}
+function MenuPesquisa() {
+    return (
+        <div className="flex gap-3 items-center h-full">
+            <div className="flex bg-neutral-800 p-2 rounded-lg gap-1 h-10/12">
+                <Icone
+                    x={40}
+                    y={40}
+                    src="/magnifying-glass-solid-full.svg"
+                    alt="Criar cifra"
+                ></Icone>
+                <input
+                    type="text"
+                    placeholder="pesquisar..."
+                    className="w-full focus:outline-0 text-white"
+                />
+            </div>
+            <button>
+                <Icone
+                    x={40}
+                    y={40}
+                    src="/sliders-solid-full.svg"
+                    alt="Filtros"
+                />
+            </button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+    );
+}
+function Cabecalho({ abrirForm }: { abrirForm: (valor: boolean) => void }) {
+    return (
+        <header className="flex gap-6 justify-center rounded-lg h-14 items-center">
+            <div
+                className=" flex rounded-full bg-cyan-700 h-fit align-middle
+                transition-transform duration-300 ease-in-out hover:scale-110 active:scale-110"
+            >
+                <button className="p-1" onClick={() => abrirForm(true)}>
+                    <Icone
+                        x={40}
+                        y={40}
+                        src="/plus-solid-full.svg"
+                        alt="Criar cifra"
+                    />
+                </button>
+            </div>
+            <MenuPesquisa />
+        </header>
+    );
+}
+function Formulario({
+    fecharForm,
+    enviarForm,
+}: {
+    fecharForm: () => void;
+    enviarForm: (e: React.SubmitEvent<HTMLFormElement>) => void;
+}) {
+    return (
+        <div
+            className="fixed inset-0 bg-black/50
+        flex flex-col justify-center items-center
+        "
+        >
+            <form
+                className="bg-zinc-800 p-6 rounded-lg flex flex-col gap-5 min-w-80"
+                onSubmit={enviarForm}
+            >
+                <header className="flex w-full justify-center">
+                    <h2 className="justify-self-center text-4xl">
+                        Adicionar Cifra
+                    </h2>
+                </header>
+                <section className="flex flex-col gap-3">
+                    <label className="flex flex-col">
+                        <span>Nome:</span>
+                        <input
+                            name="nome"
+                            type="text"
+                            placeholder="Nome da cifra"
+                            className="border-2 border-neutral-400 rounded-md p-1
+                    focus:outline-0 focus:border-blue-500 focus:border-2"
+                        ></input>
+                    </label>
+                    <label className="flex flex-col">
+                        <span>Link:</span>
+                        <input
+                            name="link"
+                            type="text"
+                            placeholder="Link da cifra"
+                            className="border-2 border-neutral-400 rounded-md p-1
+                    focus:outline-0 focus:border-blue-500 focus:border-2"
+                        ></input>
+                    </label>
+                </section>
+                <div className="flex justify-center gap-5">
+                    <button
+                        onClick={() => fecharForm()}
+                        type="button"
+                        className="rounded-md px-3 py-2 border border-neutral-400 active:brightness-90"
+                    >
+                        Cancelar
+                    </button>
+                    <button className="text-black bg-white rounded-md px-3 py-2 active:brightness-90">
+                        Adicionar
+                    </button>
+                </div>
+            </form>
         </div>
-      </main>
-    </div>
-  );
+    );
+}
+function enviarForm(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const dados: FormData = new FormData(e.currentTarget);
+    const valores: object = Object.fromEntries(dados.entries());
+    console.log(dados);
+    console.log(valores);
+}
+export default function Main() {
+    const [formulario, setFormulario] = useState(false);
+    return (
+        <main className="flex flex-col m-1 justify-center rounded-md">
+            <Cabecalho abrirForm={setFormulario} />
+
+            {formulario && (
+                <Formulario
+                    fecharForm={() => setFormulario(false)}
+                    enviarForm={(e) => {
+                        enviarForm(e);
+                        setFormulario(false);
+                    }}
+                ></Formulario>
+            )}
+        </main>
+    );
 }
