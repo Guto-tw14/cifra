85317084f07b4a82e5069e6875aedb65d6da8538
AUGUSTO WAINER <augusto.twainer@gmail.com>
2026-08-11 17:42:39 -0300

    pra continuar em casa, adicionando a pesquisa

--- app/page.tsx
+++ app/page.tsx
@@ -16,10 +16,23 @@ type cifraformData = Omit<Cifra, "id">;
 type cifraFormErrors = Partial<Record<keyof cifraformData, string>>;
 type cifraFormTouched = Partial<Record<keyof cifraformData, boolean>>;
 
-function Icone({ src, alt, x, y, className="" }: iconeProps) {
-  return <Image src={src} alt={alt} width={x} height={y} draggable={false} className={"icone"+className}/>;
+function Icone({ src, alt, x, y, className = "" }: iconeProps) {
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={x}
+      height={y}
+      draggable={false}
+      className={"icone" + className}
+    />
+  );
 }
-function MenuPesquisa() {
+function MenuPesquisa({
+  mudancaPesquisa,
+}: {
+  mudancaPesquisa: (e: ChangeEvent<HTMLInputElement>) => void;
+}) {
   return (
     <div className="flex gap-3 items-center h-full">
       <div className="flex bg-bg-card p-2 rounded-lg gap-1 h-10/12 focus-within:bg-bg-elevated border border-border-subtle">
@@ -30,6 +43,7 @@ function MenuPesquisa() {
           alt="Criar cifra"
         ></Icone>
         <input
+          onChange={mudancaPesquisa}
           type="text"
           placeholder="pesquisar..."
           className="w-full focus:outline-0 text-text-main"
@@ -41,7 +55,13 @@ function MenuPesquisa() {
     </div>
   );
 }
-function Cabecalho({ abrirForm }: { abrirForm: (valor: boolean) => void }) {
+function Cabecalho({
+  abrirForm,
+  mudancaPesquisa,
+}: {
+  abrirForm: (valor: boolean) => void;
+  mudancaPesquisa: (e: ChangeEvent<HTMLInputElement>) => void;
+}) {
   return (
     <header className="flex gap-6 justify-center rounded-lg h-14 items-center bg-bg-card p-1">
       <div
@@ -51,10 +71,16 @@ function Cabecalho({ abrirForm }: { abrirForm: (valor: boolean) => void }) {
                 "
       >
         <button className="p-1" onClick={() => abrirForm(true)}>
-          <Icone x={40} y={40} src="/plus-solid-full.svg" alt="Criar cifra" className="Branco"/>
+          <Icone
+            x={40}
+            y={40}
+            src="/plus-solid-full.svg"
+            alt="Criar cifra"
+            className="Branco"
+          />
         </button>
       </div>
-      <MenuPesquisa />
+      <MenuPesquisa mudancaPesquisa={mudancaPesquisa} />
     </header>
   );
 }
@@ -196,6 +222,20 @@ function Formulario({
   );
 }
 function ListaCifras({ cifras }: { cifras: Cifra[] }) {
+  function destacarPesquisa(texto: string, pesquisa: string){
+    if(pesquisa.trim() == ""){
+      return texto
+    }
+    const inicio = texto.toLowerCase().indexOf(pesquisa.toLowerCase())
+    const fim = inicio + pesquisa.length
+    return(
+      <span>
+        {texto.slice(0, inicio)}
+        <b>{texto.slice(inicio, fim)}</b>
+        {texto.slice(fim)}
+      </span>
+    )
+  }
   return (
     <section className="flex flex-col gap-1 text-text-main rounded-md p-1">
       {cifras.map((cifra) => (
@@ -229,9 +269,21 @@ function enviarForm(e: React.SubmitEvent<HTMLFormElement>): cifraformData {
 export default function Main() {
   const [formulario, setFormulario] = useState(false);
   const [Cifras, setCifras] = useState<Cifra[]>([]);
+  const [Pesquisa, setPesquisa] = useState("");
+  const cifrasFiltradas = Cifras.filter(
+    (cifra) =>
+      cifra.nome.toLowerCase().includes(Pesquisa.toLowerCase()) ||
+      cifra.autor?.toLocaleLowerCase().includes(Pesquisa.toLocaleLowerCase()),
+  );
+
   return (
     <main className="flex flex-col p-1 gap-1 min-h-screen bg-bg">
-      <Cabecalho abrirForm={setFormulario} />
+      <Cabecalho
+        abrirForm={setFormulario}
+        mudancaPesquisa={(e) => {
+          setPesquisa(e.target.value);
+        }}
+      />
 
       {formulario && (
         <Formulario
@@ -249,7 +301,7 @@ export default function Main() {
           }}
         ></Formulario>
       )}
-      <ListaCifras cifras={Cifras}></ListaCifras>
+      <ListaCifras cifras={cifrasFiltradas}></ListaCifras>
     </main>
   );
 }
