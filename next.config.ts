b004941fb671e02c34d1e35133f882fb1ec435c8
Guto <augusto.twainer@gmail.com>
2026-08-11 20:08:51 -0300

    github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export'
+  output: "export",
+  basePath: "/cifra",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
