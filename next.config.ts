4f0b6f7dd3a6f8c8a5eb9c7bc2a6fa12ed204ebb
AUGUSTO WAINER <augusto.twainer@gmail.com>
2026-08-17 16:30:06 -0300

    basepath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     output: 'export',
     //comentar para rodar local: basePath: '/cifra',
-    // basePath: '/cifra',
+    basePath: '/cifra',
     images: {
         unoptimized: true,
     },
