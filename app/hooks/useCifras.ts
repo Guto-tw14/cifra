35ff7df81f3c78ec393e79727a31cfd5805611bb
guto <augusto.twainer@gmail.com>
2026-08-28 10:56:59 -0300

    suporta links sem protocolo e corrige script dev

--- app/hooks/useCifras.ts
+++ app/hooks/useCifras.ts
@@ -1,34 +1,38 @@
-import { useEffect, useState } from 'react';
-import { db } from '@/app/db';
-import { type cifraFormData } from '@/app/types';
-import { type cifra } from '@/app/types';
+import { useEffect, useState } from "react";
+import { db } from "@/app/db";
+import { type cifraFormData } from "@/app/types";
+import { type cifra } from "@/app/types";
 
 export function useCifras() {
-    const [cifras, setCifras] = useState<cifra[]>([]);
+  const [cifras, setCifras] = useState<cifra[]>([]);
 
-    useEffect(() => {
-        db.cifras.toArray().then((cifras) => {
-            setCifras(cifras);
-        });
-    }, []);
+  useEffect(() => {
+    db.cifras.toArray().then((cifras) => {
+      setCifras(cifras);
+    });
+  }, []);
 
-    async function addCifra(values: cifraFormData) {
-        const cifra: cifra = {
-            id: crypto.randomUUID(),
-            name: values.name,
-            link: values.link,
-            autor: values.autor || 'desconhecido',
-        };
-        await db.cifras.add(cifra);
-
-        setCifras((old) => [...old, cifra]);
+  async function addCifra(values: cifraFormData) {
+    let link = values.link;
+    if (!link.includes("https://") && !link.includes("http://")) {
+      link = "http://" + link;
     }
+    const cifra: cifra = {
+      id: crypto.randomUUID(),
+      name: values.name,
+      link: link,
+      autor: values.autor || "desconhecido",
+    };
+    await db.cifras.add(cifra);
 
-    async function deleteCifra(id: string) {
-        db.cifras.delete(id);
+    setCifras((old) => [...old, cifra]);
+  }
 
-        setCifras((old) => old.filter((cifra) => id != cifra.id));
-    }
+  async function deleteCifra(id: string) {
+    db.cifras.delete(id);
 
-    return { cifras, addCifra, deleteCifra };
+    setCifras((old) => old.filter((cifra) => id != cifra.id));
+  }
+
+  return { cifras, addCifra, deleteCifra };
 }
